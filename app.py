f63cee95a608f244f0732f894ff2eaf4d5e36fd7
Raihan Fadillah <105834561+Raihan14-Fadillah@users.noreply.github.com>
2025-05-21 09:25:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -117,8 +117,5 @@ def text_to_speech_indonesia(kalimat):
     
     return hasil, audio_data
 
-import os
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
